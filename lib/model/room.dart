82bb6c11ce63ae0766c8102c95b8bff1c8ddb96c
NickAp02 <nicoleapaflo@gmail.com>
2022-06-15 19:15:44 +0000

    creation des cards des devices, creation des modeles

--- lib/model/room.dart
+++ lib/model/room.dart
@@ -2,18 +2,19 @@ class Room{
   String idRoom = "";
   String name = "";
   double conso = 0.0;
-
-  Room(this.idRoom, this.name, this.conso);
+  String categorie = "";
+  Room(this.idRoom, this.name, this.conso,this.categorie);
 
   Room.fromJson(Map<String, dynamic> json):
     idRoom = json['id'] as String,
     name = json['name'] as String,
-    conso = json['appareils']['conso'] as dynamic;
-
+    conso = json['appareils']['conso'] as dynamic,
+    categorie = json['categorie'] as String;
 
   Map<String, dynamic> toJson() => {
     'id': idRoom,
     'name': name,
-    'conso' : conso
+    'conso' : conso,
+    'categorie' : categorie
   };
 }
